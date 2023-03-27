500c4d195ba4b9e3f99c0dae3162dcf633de83c6
FMendezSlc <fmendezslc@gmail.com>
2023-03-26 18:53:31 -0600

    draw basic figures for deck 1 and draw them in win

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from psychopy import visual, core  # import some libraries from PsychoPy
- 
+from pathlib import Path
  #create a window
-mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
+win = visual.Window([800,600], monitor="testMonitor", units="deg")
  
+path =  '/Users/labc02/Documents/GitHub/Set_Recognition/pics/'
 #create some stimuli
-grating = visual.GratingStim(win=mywin, mask="circle", size=3, pos=[-4,0], sf=3)
-fixation = visual.GratingStim(win=mywin, size=0.5, pos=[0,0], sf=0, rgb=-1)
+card_1 = visual.ImageStim(win, image = path+'ofbo.png', size = 5, pos = (-7, 0))
+card_2 = visual.ImageStim(win, image = path+'ofbr.png', size = 5, pos = (0, 0))
+card_3 = visual.ImageStim(win, image = path+'ofbt.png', size = 5, pos = (7, 0))
  
 #draw the stimuli and update the window
-grating.draw()
-fixation.draw()
-mywin.update()
+card_1.draw()
+card_2.draw()
+card_3.draw()
+win.update()
 
 #pause, so you get a chance to see it!
 core.wait(5.0)
