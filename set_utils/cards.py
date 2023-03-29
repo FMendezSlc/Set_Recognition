0814ea99956203c63ddf4f98b76746d8fba19ab6
FMendezSlc <fmendezslc@gmail.com>
2023-03-28 18:44:21 -0600

    added pics and definitions for deck 2

--- set_utils/cards.py
+++ set_utils/cards.py
@@ -12,3 +12,12 @@ ofbo = Card(shape = 'oval', filling = 'solid', color = 'black', number = 'one', 
 ofbr = Card(shape = 'rectangle', filling = 'solid', color = 'black', number = 'one', picture = 'ofbr.png')
 
 deck_1 = [ofbd, ofbo, ofbr]
+
+oebd = Card(shape = 'diamond', filling = 'empty', color = 'black', number = 'one', picture = 'oebd.png')
+oebo = Card(shape = 'oval', filling = 'empty', color = 'black', number = 'one', picture = 'oebo.png')
+oebr = Card(shape = 'rectangle', filling = 'empty', color = 'black', number = 'one', picture = 'oebr.png')
+osbd = Card(shape = 'diamond', filling = 'striped', color = 'black', number = 'one', picture = 'osbd.png')
+osbo = Card(shape = 'oval', filling = 'striped', color = 'black', number = 'one', picture = 'osbo.png')
+osbr = Card(shape = 'rectangle', filling = 'striped', color = 'black', number = 'one', picture = 'osbr.png')
+
+deck_2 = deck_1 + [oebd, oebo, oebr, osbd, osbo, osbr]
