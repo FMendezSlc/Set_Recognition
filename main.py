0a26a99f5de7727ffa71cd82d3128b74e08bba4f
FMendezSlc <fmendezslc@gmail.com>
2023-04-18 16:22:56 -0600

    added polarised method to prepare rounds

--- main.py
+++ main.py
@@ -30,12 +30,17 @@ functions.instructions(win)
 #Examples
 functions.examples(win, path)
 
-conditions = {'n_trails': 50, 'iti': 0.0, 'holding': 10, 'timeOut': 5}
+conditions = {'Standard' : {'n_trials': 50, 'iti': 0.0, 'holding': 10, 'timeOut': 5},
+              'Polarised' :{'n_trials': 60, 'iti': 0.0, 'holding': 10, 'timeOut': 5}
+              }
+
+exp_conditions = conditions[exp_info['experiment_type']]
+
 block = 0
 
 for deck in cards.decks:
 
-    hands = functions.prepare_rounds(deck, prop_sets= 0.5, rounds= conditions['n_trails'])
+    hands = functions.prepare_rounds(deck, prop_sets= 0.5, rounds= exp_conditions['n_trials'])
     block += 1
     trialHandler = data.TrialHandler(trialList = [{'hand': hand} for hand in hands], nReps= 1, dataTypes= ['block', 'correctResp', 'setLevel', 'userResp', 'evaluation', 'RT'], extraInfo=exp_info) # object to control trial config and data storage
 
@@ -61,7 +66,6 @@ for deck in cards.decks:
         msg_2 = visual.TextStim(win, text = 'Tap on the spacebar if this is a Set.\nPress Shift if it\'s not.', 
                                 pos = (0, -5), height= 0.7)
 
-        # --> inter-trial interval goes here if necessary <--
 
         #draw the stimuli and update the window
         msg_1.draw()
@@ -69,19 +73,23 @@ for deck in cards.decks:
         card_2.draw()
         card_3.draw()
         msg_2.draw()
+
+        # --> inter-trial interval can be modified in conditions dic 'iti' <--
+        core.wait(exp_conditions['iti'])
+
         win.flip()
 
         response_clock = core.Clock() # start the clock
 
-        # --> holding time can be modified here <--
-        responses = event.waitKeys(10, timeStamped = response_clock, keyList = ['space', 'rshift', 'lshift']) # wait for the response and record it
+        # --> holding time can be modified in conditions dic 'holding' <--
+        responses = event.waitKeys(exp_conditions['holding'], timeStamped = response_clock, keyList = ['space', 'rshift', 'lshift']) # wait for the response and record it
         if responses == None:
             trialHandler.addData('userResp', 'Missed')
             msg_missed = visual.TextStim(win, text = 'You missed the trial!!', pos = (0, 0), height = 1.5)
             msg_missed.draw()
             win.flip()
             trialHandler.addData('evaluation', 'omission')
-            core.wait(0.3)
+            core.wait(0.5)
         else:
             if responses[0][0] == 'space' :
                 user_answer = True
@@ -96,13 +104,13 @@ for deck in cards.decks:
                 msg_correct.draw()
                 win.flip()
                 trialHandler.addData('evaluation', 'correct')
-                core.wait(0.3)
+                core.wait(0.5)
             else:  
                 msg_wrong = visual.TextStim(win, text = 'Wrong!!\n:(', color = '#FF6666', pos = (0, 0), height = 1.5)
                 msg_wrong.draw()
                 win.flip()
                 trialHandler.addData('evaluation', 'incorrect')
-                core.wait(0.4)
+                core.wait(0.5)
             #print(user_answer)
 
         current_exp.nextEntry()
